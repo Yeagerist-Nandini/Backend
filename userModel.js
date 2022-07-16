a4844594b0c5e3a644a1c3ed82179123e91d163b
Nandini Jaiswal <77235640+Yeagerist-Nandini@users.noreply.github.com>
2022-07-17 00:50:08 +0530

    Add files via upload

---
+++ userModel.js
@@ -0,0 +1,51 @@
+//tech knowledge  
+// (schema) -> set of features and rules a certain entity should 
+// follow
+// * how to create a db ->  link share
+// connect to my app // mongoose 
+const mongoose = require('mongoose'); //npm i mongoose
+let dblink = "mongodb+srv://Nandini:<password>@cluster0.swaac.mongodb.net/?retryWrites=true&w=majority";
+// db connect
+mongoose
+    .connect(dblink)
+    .then(function () {
+        console.log("connected");
+    })
+    .catch(function (err) {
+        console.log("error", err);
+    })   
+// how to create a schema-> only entries written will be added to your db no one else 
+let userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phonenumber: {
+        type: "String",
+        minLength: 10,
+        maxLength: 10
+    },
+    pic: {
+        type: String,
+        default: "dp.png"
+    },
+    address: {
+        type: String,
+    }
+})
+
+const UserModel = mongoose.model('FooduserModel', userSchema);
+module.exports = UserModel;
